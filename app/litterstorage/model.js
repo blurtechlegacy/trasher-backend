b08f60131cbe5b1d1b0f2b949952ab8e8c86f243
AlexBulatov <al.ross@yandex.ru>
2019-09-27 22:09:58 +0700

    Define litterstorage model

--- app/litterstorage/model.js
+++ app/litterstorage/model.js
@@ -1,3 +1,30 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+
+const definition = {
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude:{
+    type: Number,
+    required: true
+  },
+  lastCollect: {
+    type: Date,
+    default: Date.now()
+  },
+  place: {
+    city: {
+      type: String
+    },
+    region: {
+      type: String
+    }
+  }
+};
+
+const UserSchema = new Schema(definition, {});
+
+module.exports = mongoose.model('Litter', UserSchema, "Litter");
